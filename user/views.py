26bb36e3810ed1f20d863bd92c7ab7713615bac1
정형빈 <gudqls369@gmail.com>
2022-10-14 20:19:27 +0900

    최종수정완료

--- user/views.py
+++ user/views.py
@@ -7,23 +7,22 @@ def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html')
     elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        phone = request.POST.get('phone', None)
-        address = request.POST.get('address', None)
-
-        new_user = User()
-        new_user.username = username
-        new_user.password = password
-        new_user.phone = phone
-        new_user.address = address
-        new_user.save()
-        return redirect('/login')
+        user = User()
+        
+        user.username = request.POST.get('username', '')
+        user.phone = request.POST.get('phone', '')
+        user.address = request.POST.get('address', '')
+        
+        user.set_password(request.POST.get('password', ''))
+        
+        user.save()
+        
+        return redirect('/login/')
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
 
         me = auth.authenticate(request, username=username, password=password)
         if me is not None:
